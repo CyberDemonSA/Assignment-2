e2908ba929433c60a5f6c43a8d72a77cc35cd78d
User <230519@astanait.edu.kz>
2024-01-25 19:01:04 +0600

    first init

---
+++ src/Person.java
@@ -0,0 +1,36 @@
+public class Person {
+    private int id;
+    private static int id_gen;
+    private String name;
+    private String surname;
+
+    public Person(){
+        id=id_gen++;
+    }
+
+    public Person(String name, String surname){
+        this();
+        setName(name);
+        setSurname(surname);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() { return surname; }
+
+
+}
